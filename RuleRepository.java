70ca3b8daffe42faf2bdb79fcd9f7b0b70989305
Vikash Vishwakarma <36168483+vikashkv@users.noreply.github.com>
2024-10-27 22:14:25 +0530

    Delete RuleRepository.java

--- RuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Rule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RuleRepository extends JpaRepository<Rule, Integer> {
-}
